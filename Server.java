93d63614765c977aabb5f86893e9a3e258c334f6
Miguel Palma <migueljesus_palma@yahoo.com.ph>
2015-09-25 14:05:12 +0800

    puuuuush
    lol

--- Server.java
+++ Server.java
@@ -1,3 +1,7 @@
+/*
+* Server program for Trust Manager
+*/
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -7,21 +11,21 @@ public class Server {
 
   public static void main(String[] args) throws Exception {
     HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-    server.createContext("/test", new HttpGetHandler());
+    server.createContext("/create", new ClientConfig());
     server.setExecutor(null); // creates a default executor
     server.start();
     System.out.println("Server started");
   }
 
-  static class HttpGetHandler implements HttpHandler {
+  static class ClientConfig implements HttpHandler {
     public void handle(HttpExchange t) throws IOException {
       String req = t.getRequestMethod();
+      System.out.println("Method: "+req);
       Scanner sc = new Scanner(t.getRequestBody());
       while(sc.hasNextLine()){
         System.out.println(sc.nextLine());
       }
-      System.out.println("Input Read");
-      String response = "hellooooo";
+      String response = "Response from server";
       t.sendResponseHeaders(200, response.length());
       OutputStream os = t.getResponseBody();
       os.write(response.getBytes());
