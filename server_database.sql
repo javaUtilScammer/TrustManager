5e46d15f46c7bf3c1bf597eb0296318f74a63947
hadrianang <hadrianang@outlook.com>
2015-09-25 18:01:03 +0800

    Changed database stuff

--- server_database.sql
+++ server_database.sql
@@ -3,8 +3,10 @@ USE ClientServers;
 
 CREATE TABLE Clients(
 	client_id int AUTO_INCREMENT,
-	key varchar(35) UNIQUE NOT NULL,
+	key_word varchar(35) UNIQUE NOT NULL,
 	validation_time int NOT NULL, 
 	default_score double NOT NULL, 
-	rating_scale double NOT NULL
+	rating_scale double NOT NULL,
+
+	PRIMARY KEY(client_id)
 );
