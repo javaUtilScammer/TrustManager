9d2dae0dda8a7caa2ee52ec6225f1028917eae3f
Hadrian Ang <hadrianang@outlook.com>
2015-12-29 16:39:55 +0800

    Finished documentation of my parts

--- ThesisSystem/src/Validator.java
+++ ThesisSystem/src/Validator.java
@@ -1,6 +1,10 @@
 import java.sql.Connection;
 import java.util.ArrayList;
 
+/**
+ * abstract Validator class for different Validator functions used by the system
+ * @author hadrianang
+ */
 public abstract class Validator {
 
     final double rating_scale, score_validity,degree_of_strictness, beta_factor, active_user_time, active_evaluation_time;
@@ -15,6 +19,11 @@ public abstract class Validator {
         active_user_time = intrface.getActiveUserTime();
         active_evaluation_time = intrface.getActiveEvaluationTime(); 
     }
-
+    
+    /**
+     * main Validation function to be used by this Validator
+     * @param ev the Evaluation submitted and was recently used to modify the score of a Contrbution
+     * @return whether or not the Contribution rated by the Evaluation has reached the threshold computed
+     */
     public abstract boolean validate(Evaluation ev);
 }
